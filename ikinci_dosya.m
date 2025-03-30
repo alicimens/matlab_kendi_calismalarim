4ee064814b9db1c5756f79f5d4a69eb9bdfd2996
alicimens <115820400+alicimens@users.noreply.github.com>
2025-03-30 18:04:32 +0300

    Add files via upload

---
+++ ikinci_dosya.m
@@ -0,0 +1,10 @@
+number  = 123 
+
+x = 123 +1  ;
+ 
+
+b = sind(90)
+
+rand %  bu  keyword 0 ile  1  arasında  random  sayi verir 
+
+rng(3) %  'random number generator' açılımı  budur.. rastgele  sayiların aynı  kalmasını sağlar 
---
+++ ilk_dosya.m
@@ -0,0 +1,11 @@
+
+
+
+%matlabde  işlem  önceliği sırası
+
+% parantezler 
+% üstel  işlemler 
+%  çarpma / bölme 
+%  toplama  /  çıkarma 
+
+6 *(10/13) + ( 18/ (5*7)) + ((9^2) *5)
